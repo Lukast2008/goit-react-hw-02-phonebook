e6da72f18270696d61b9c384864e24b3d12cab6c
Lukast2008 <lukast2008@gmail.com>
2022-12-09 19:07:17 +0200

    done Hw 2 react

---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,20 @@
+import styles from "../Phonebook/styles.module.css"
+import PropTypes from 'prop-types';
+
+export const ContactList = ({ contact, contactDelete }) => {
+  return (
+    <ul className={styles.list}>
+      {contact.map(el => (
+        <li className={styles.listItem} id={el.id} key={el.id}>
+          {el.name}: {el.number}
+          <button className={styles.listButton} type="button" onClick={() =>contactDelete(el.id)}>Delete</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+ContactList.propTypes = {
+  filterContacts: PropTypes.func,
+  deleteContact: PropTypes.func,
+}
